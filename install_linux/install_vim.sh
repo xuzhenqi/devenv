837a9286f15cf460a57d5a9b2fb5f45b5fb6b859
xuzhenqi <xuzhenqi1993@gmail.com>
2017-01-08 18:14:01 +0800

    Adding install vim from source

--- install_linux/install_vim.sh
+++ install_linux/install_vim.sh
@@ -1,17 +1,27 @@
+# install vim from source
+cd $PWD/..
+git clone git@github.com:vim/vim.git
+cd vim/src
+export LDFLAGS="-L${PWD}/../../local/lib"
+export CPPFLAGS="-I${PWD}/../../local/include"
+./configure --with-features=huge --enable-multibyte --enable-pythoninterp=yes --enable-cscope --prefix=${PWD}/../../local
+make -j`nproc`
+make install
+cd $PWD
+
+# if [ "$INSTALL_VIM" = "" ]; then
+#   echo "Using avaliable vim"
+# elif [ "$INSTALL_VIM" = "Apt" ]; then
+#   # install vim8
+#   sudo apt-get install software-properties-common
+#   sudo add-apt-repository ppa:jonathonf/vim
+#   sudo apt-get update
+#   sudo apt-get install vim
+# elif [ "$INSTALL_VIM" = "Source" ]; then
+#   echo "Todo"
+# fi
+
 git submodule update --init --recursive
-
-if [ "$INSTALL_VIM" = "" ]; then
-  echo "Using avaliable vim"
-elif [ "$INSTALL_VIM" = "Apt" ]; then
-  # install vim8
-  sudo apt-get install software-properties-common
-  sudo add-apt-repository ppa:jonathonf/vim
-  sudo apt-get update
-  sudo apt-get install vim
-elif [ "$INSTALL_VIM" = "Source" ]; then
-  echo "Todo"
-fi
-
 rm -rf ~/.vim ~/.vimrc
 ln -sf $PWD/source/vim-pathogen/autoload .vim/autoload
 if [ ! -d '.vim/indent' ]; then
