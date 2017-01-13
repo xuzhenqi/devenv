a606b22eb5127f2dc534c4258c520f0fa2580ac6
xuzhenqi <xuzhenqi1993@gmail.com>
2017-01-13 16:23:14 +0800

    Adding parallel.sh

--- install_linux.sh
+++ install_linux.sh
@@ -9,3 +9,4 @@ sh install_linux/install_unzip.sh
 sh install_linux/install_tmux.sh
 sh install_linux/install_libx264.sh
 sh install_linux/install_ffmpeg.sh
+sh install_linux/install_parallel.sh
---
+++ install_linux/install_parallel.sh
@@ -0,0 +1,11 @@
+PARALLEL=`which parallel`
+if [ "$PARALLEL" = "" ]; then
+    if [ ! -d "$DEV_HOME/devlopment/parallel-20161222" ]; then
+        wget -O $DEV_HOME/development/parallel-20161222.tar.bz2 http://ftp.gnu.org/gnu/parallel/parallel-20161222.tar.bz2
+        tar -jxf $DEV_HOME/development/parallel-20161222.tar.bz2 -O $DEV_HOME/development/parallel-20161222
+    fi
+    cd $DEV_HOME/development/parallel-20161222
+    ./configure --prefix=$DEV_HOME/local
+    make -j`nproc`
+    make install
+fi
