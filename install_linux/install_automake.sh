745048bb634cbf6d207bc1c367823724525464eb
xuzhenqi <xuzhenqi1993@gmail.com>
2017-04-06 14:25:06 +0800

    Adding automake

---
+++ install_linux/install_automake.sh
@@ -0,0 +1,12 @@
+AUTOMAKE=`which automake`
+if [ "$AUTOMAKE" = "" ]; then
+    cd $DEV_HOME/development
+    if [ ! -d automake-1.15 ]; then
+        wget http://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz
+        tar -xvf automake-1.15.tar.gz
+    fi
+    cd automake-1.15
+    ./configure --prefix=$DEV_HOME/local
+    make -j`nproc`
+    make install
+fi
