2bd4b0ba15dab56dc0fe4799c2da79b5b55aa055
xuzhenqi <xuzhenqi1993@gmail.com>
2017-10-16 11:13:55 +0800

    Editing tmux.conf; Adding install_libmagick.sh

---
+++ install_linux/install_libmagick.sh
@@ -0,0 +1,10 @@
+cd $DEV_HOME/development
+if [ ! -d ImageMagick-7.0.7-8 ]; then
+    # TODO(xzq): using specific version url
+    wget https://www.imagemagick.org/download/ImageMagick.tar.gz -O ImageMagick.tar.gz
+    tar -xf ImageMagick.tar.gz
+fi
+cd ImageMagick-7.0.7-8
+./configure --prefix=$DEV_HOME/local
+make -j`nproc`
+make install
