87aa2801dee7cf877d62bd7779cac3803a16562f
xuzhenqi <xuzhenqi1993@gmail.com>
2018-08-07 16:47:09 +0800

    remove -z

--- install_linux/install_ctags.sh
+++ install_linux/install_ctags.sh
@@ -2,7 +2,7 @@ set -e
 cd $DEV_HOME/development
 if [ ! -d ctags-5.8 ]; then
 wget http://prdownloads.sourceforge.net/ctags/ctags-5.8.tar.gz
-tar -xzf ctags-5.8.tar.gz
+tar -xf ctags-5.8.tar.gz
 fi
 cd ctags-5.8
 ./configure --prefix=$DEV_HOME/local
